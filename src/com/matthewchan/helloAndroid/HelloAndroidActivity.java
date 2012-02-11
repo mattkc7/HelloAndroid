abf07a1677948f77790f09e2333e169fe8fad700
Matthew K. Chan <mattkc7@gmail.com>
2012-02-11 08:06:51 -0800

    playing with ui widgets

--- src/com/matthewchan/helloAndroid/HelloAndroidActivity.java
+++ src/com/matthewchan/helloAndroid/HelloAndroidActivity.java
@@ -2,12 +2,63 @@ package com.matthewchan.helloAndroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
+import java.util.Date;
+
+import com.matthewchan.helloAndroid.R; //.yamba.R;
+
+public class HelloAndroidActivity extends Activity implements View.OnClickListener{
+    Button btn;
+    Button sizeBtn;
+    Boolean tf = true;
+    EditText editText;
+    //ImageView img;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        
+        sizeBtn =  (Button)findViewById(R.id.button1);
+        sizeBtn.setOnClickListener(this);
+        updateTime();
+        
+        //img = (ImageView) findViewById(R.id.icon);
+        editText = (EditText) findViewById(R.id.field);
+//        btn = new Button(this);
+//        btn.setTextSize(30);
+//        btn.setOnClickListener(this);
+//        updateTime();
+//        setContentView(btn);
+    }
+    
+    public void onClick(View v){
+    	if (v == btn){
+    		updateTime();
+    	} else {
+    		changeColor(v);
+    	}
+    }
+    
+    private void updateTime(){
+    	btn.setText(new Date().toString());
+
+    }
+    
+    private void changeColor(View theBtn){
+    	if (tf){
+    		btn.setHeight(220);
+    	} else {
+    		btn.setHeight(150);
+    	}
+    	
+    	tf = !tf;
     }
 }
